bd624b5852b826afabfa1188a8152bcfcb27dc70
Gal Shvarzman <gal.shvarzman@gmail.com>
2018-04-27 17:31:27 +0300

    linked list

---
+++ day-5/index.js
@@ -0,0 +1,77 @@
+class Node{
+    constructor(value){
+        this.data = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    add(node){
+        if(this.length){
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        else{
+            this.head = node;
+            this.tail = node;
+        }
+        this.length ++;
+        console.log("Node added successfully to the list");
+        return node;
+    }
+    search(position){
+        if(this.length && position < this.length){
+            let current = this.head;
+            const moves = position -1;
+            for(let i = 0; i !== moves; i++){
+                current = current.next;
+            }
+            return current;
+        }
+        else{
+            throw new Error("The list is empty or the node do not exist");
+        }
+    }
+    remove(position){
+        try{
+            const node = this.search(position);
+            const next = node.next;
+            const prev = node.prev;
+
+            next.prev = prev;
+            prev.next = next;
+
+            this.length--;
+            console.log("Node removes successfully");
+            return this;
+        }
+        catch(err){
+            throw new Error("Can not remove node because the list is empty or the node do not exist");
+        }
+    }
+    print(){
+        let current = this.head;
+        console.log(current.data);
+        for(let i = 1; i < this.length; i++){
+            current = current.next;
+            console.log(current.data);
+        }
+    }
+}
+
+const myList = new LinkedList();
+
+myList.add(new Node("one"));
+myList.add(new Node("two"));
+myList.add(new Node("three"));
+myList.add(new Node("four"));
+myList.add(new Node("five"));
+
+myList.print();
